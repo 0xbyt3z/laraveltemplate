ccaa9b58ece3d8695b3f6328aea70631c5b8b579
theekshana <rtheekshanafernando@gmail.com>
2023-08-08 10:43:35 +0530

    changes

--- resources/js/Pages/home.jsx
+++ resources/js/Pages/home.jsx
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
+import * as React from "react";
 
-const Test = () => {
-    return <h1 className="text-xl">This is the Home</h1>;
-};
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 
-export default Test;
+export default function SelectDemo() {
+    return <div className="bg-pink-400 w-screen h-screen"></div>;
+}
